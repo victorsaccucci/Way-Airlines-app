8e9e1057deb792420abd5a069992d64cdd5f7467
victorsaccucci <victorsaccucciv@gmail.com>
2024-10-04 18:13:12 -0300

    Projeto criado

---
+++ Way Airlines app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Way Airlines app
+//
+//  Created by Victor Saccucci on 04/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Way Airlines app/Way_Airlines_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Way_Airlines_appApp.swift
+//  Way Airlines app
+//
+//  Created by Victor Saccucci on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Way_Airlines_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
